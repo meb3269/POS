7a6fcbbfc7e6f39b28c17bc2c0940a7c11ba093c
meblack <meblack@4000180L>
2017-03-23 14:36:12 -0400

    Added new classes to the Class Library

---
+++ PosLibraryCode/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PosLibraryCode
+{
+	public class Employee
+	{
+		public string FirstName { get; set; }
+
+	}
+}
